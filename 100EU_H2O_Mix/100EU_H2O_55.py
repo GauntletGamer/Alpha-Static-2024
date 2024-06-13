558cab81b97e0aa96a81d764d9c76c3ce3f4c732
David Leep <85815208+Leep-D@users.noreply.github.com>
2024-06-12 17:25:45 -0700

    Add files via upload

---
+++ 100EU_H2O_Mix/100EU_H2O_55.py
@@ -0,0 +1,45 @@
+import openmc
+
+# Create 100% Enriched Uranium in H2O
+u = openmc.Material(name="Uranium 100%")
+u.set_density('g/cm3', 19.05)
+u.add_nuclide('U235', 1)
+water = openmc.Material(name='Water',material_id=3)
+water.set_density('g/cm3',1.0)
+water.add_element('O',1.0)
+water.add_element('H',2.0)
+UWP = 0.055/(0.055+1.0)
+UH2O = openmc.Material.mix_materials([u,water],[UWP,1-UWP],percent_type='wo',name="U_H2O_Mix")
+mats = openmc.Materials([UH2O,water])
+mats.export_to_xml()
+
+# Create Homogenous Mix Sphere
+    # Critical Radius: 15.281
+    # K_eff=0.95 Radius: 14.165
+    # K_eff=0.98 Radius: 14.814
+    # 75% CM Radius: 13.884
+fuel_rad = 13.882
+sphere = openmc.Sphere(r=fuel_rad)
+reflect_sp = openmc.Sphere(r=fuel_rad+30, boundary_type='vacuum')
+cell = openmc.Cell(fill=UH2O, region=-sphere)
+cell2 = openmc.Cell(fill=water, region=-reflect_sp & +sphere)
+geom = openmc.Geometry([cell,cell2])
+geom.export_to_xml()
+
+# Define run settings
+settings = openmc.Settings()
+settings.batches = 150
+settings.inactive = 50
+settings.particles = 500000
+settings.alpha_mode = True
+settings.prompt_only = True
+settings.export_to_xml()
+
+# Run Simulation
+openmc.run()
+
+# Get resulting k-eff or α-eff value
+n = settings.batches
+with openmc.StatePoint(f'statepoint.{n}.h5') as sp:
+    alpha_eff = sp.alpha_eff
+    print(f'Final α-effective = {alpha_eff}')
