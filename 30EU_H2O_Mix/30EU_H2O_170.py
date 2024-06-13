a032d5aa58ef45977692cd97f9d340fd81c8c6dd
David Leep <85815208+Leep-D@users.noreply.github.com>
2024-06-12 17:24:44 -0700

    Add files via upload

---
+++ 30EU_H2O_Mix/30EU_H2O_170.py
@@ -0,0 +1,45 @@
+import openmc
+
+# Create 100% Enriched Uranium in H2O
+u = openmc.Material(name="Uranium 30%")
+u.set_density('g/cm3',19.05)
+u.add_element('U', 100, enrichment=30)
+water = openmc.Material(name='Water',material_id=3)
+water.set_density('g/cm3',1.0)
+water.add_element('O',1.0)
+water.add_element('H',2.0)
+UWP = 0.17/(0.17+1.0)
+UH2O = openmc.Material.mix_materials([u,water],[UWP,1-UWP],percent_type='wo',name='U_H2O_Mix')
+mats = openmc.Materials([UH2O,water])
+mats.export_to_xml()
+
+# Create Homogenous Mix Sphere
+    # Critical Radius: 16.704
+    # K_eff=0.95 Radius: 15.392
+    # K_eff=0.98 Radius: 16.154
+    # 75% CM Radius: 15.177
+fuel_rad = 15.177
+sphere = openmc.Sphere(r=fuel_rad)
+reflect_sp = openmc.Sphere(r=fuel_rad+30, boundary_type='vacuum')
+cell = openmc.Cell(fill=UH2O, region=-sphere)
+cell2 = openmc.Cell(fill=water, region=-reflect_sp & +sphere)
+geom = openmc.Geometry([cell,cell2])
+geom.export_to_xml()
+
+# Define run settings
+settings = openmc.Settings()
+settings.batches = 150
+settings.inactive = 50
+settings.particles = 500000
+settings.alpha_mode = True
+settings.prompt_only = True
+settings.export_to_xml()
+
+# Run Simulation
+openmc.run()
+
+# Get resulting k-eff or α-eff value
+n = settings.batches
+with openmc.StatePoint(f'statepoint.{n}.h5') as sp:
+    alpha_eff = sp.alpha_eff
+    print(f'Final α-effective = {alpha_eff}')
---
+++ 30EU_H2O_Mix/Benchmark_keff.py
@@ -0,0 +1,42 @@
+import openmc
+import openmc.model
+import numpy as np
+import matplotlib.pyplot as plt
+
+mats = openmc.Materials.from_xml(path='materials.xml')
+fuel = mats[0]
+water = mats[1]
+print(fuel)
+print(water)
+def build_model(radius):
+    # Create a single cell filled with the Pu metal
+    sphere = openmc.Sphere(r=radius)
+    reflect_sph = openmc.Sphere(r=radius+30, boundary_type='vacuum')
+    cell = openmc.Cell(fill=fuel, region=-sphere)
+    cell2 = openmc.Cell(fill=water, region=-reflect_sph & +sphere)
+    geom = openmc.Geometry([cell,cell2])
+
+    # Define run settings
+    setts = openmc.Settings()
+    setts.batches = 150
+    setts.inactive = 50
+    setts.particles = 20000
+    setts.output = {'tallies': False}
+
+    model = openmc.model.Model(geometry=geom,materials=mats,settings=setts)
+
+    return model
+
+Target_keff = float(input("Input Desired keff: "))
+
+Conv_r, guesses, keffs = openmc.search_for_keff(build_model,bracket=[10,20],target=Target_keff,tol=1e-5,print_iterations=True,run_args={'output':False})
+
+print(Conv_r)
+print(guesses)
+
+plt.scatter(guesses,[keffs[i].nominal_value for i in range(len(keffs))])
+plt.scatter(Conv_r,Target_keff)
+plt.savefig("Radius_Convergence_{}.png".format(Target_keff))
+
+for i in range(len(guesses)):
+    print("{}\t{}".format(guesses[i],keffs[i].nominal_value))
